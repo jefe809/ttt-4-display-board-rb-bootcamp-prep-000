0aaa96dc59eb2af66e44797a619ebde7c2ed9291
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-17 01:51:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |#{board[4]}   |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  
+  
   
   
   
